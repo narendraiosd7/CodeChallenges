d1f5b956a000f05cfe3bc25a3ee3ad987f442182
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-08-23 10:22:31 +0530

    Remainder

---
+++ CodeChallenges/Remainder.swift
@@ -0,0 +1,19 @@
+//
+//  Remainder.swift
+//  CodeChallenges
+//
+//  Created by Vadde Narendra on 8/23/20.
+//  Copyright © 2020 Narendra Vadde. All rights reserved.
+//
+
+import UIKit
+
+class Remainder {
+
+    //MARK:- Return the Remainder from Two Numbers
+    
+    func remainder(_ x: Int, _ y: Int) -> Int {
+        return x % y
+    }
+    
+}
