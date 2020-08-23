8c22b78893bddc611c99d16bc746c5e7164137e8
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-08-23 10:52:11 +0530

    Converting of time

---
+++ CodeChallenges/Time Converter.swift
@@ -0,0 +1,18 @@
+//
+//  Time Converter.swift
+//  CodeChallenges
+//
+//  Created by Vadde Narendra on 8/23/20.
+//  Copyright © 2020 Narendra Vadde. All rights reserved.
+//
+
+import UIKit
+
+class Time_Converter {
+
+    //MARK:- Convert Minutes into Seconds
+    
+    func convert(_ minutes: Int) -> Int {
+        return (minutes * 60)
+    }
+}
